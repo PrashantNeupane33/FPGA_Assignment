086469f04c0cdc64fac13c71c86f81e759d4d269
PrashantNeupane33 <www.panbr123@gmail.com>
2026-06-26 22:18:03 +0545

    ALU working properly now, fixed some issue regarding INC,DEC and SUB carry flag

--- alu.v
+++ alu.v
@@ -6,12 +6,14 @@ module ALU(
 	output reg [2:0]flag //{Zero, Carry, Parity}
 );
 
-	wire [7:0] add_result, sub_result, left_shift_result, right_shift_result, cmp_result;
-	wire add_carry, sub_carry;
+	wire [7:0] add_result, sub_result, inc_result, dec_result, left_shift_result, right_shift_result, cmp_result;
+	wire add_carry, sub_carry, inc_carry, dec_carry;
 
 	// checkParity parity(.in(result), .out(flag[0]));
 	bit8adder adder(.a(operand1), .b(operand2), .sum(add_result), .cin(1'b0), .cout(add_carry));
 	bit8subtractor subtractor(.a(operand1), .b(operand2), .diff(sub_result), .cin(1'b1), .cout(sub_carry));
+	bit8adder INC(.a(operand1), .b(8'b1), .sum(inc_result), .cin(1'b0), .cout(inc_carry));
+	bit8subtractor DEC(.a(operand1), .b(8'b1), .diff(dec_result), .cin(1'b1), .cout(dec_carry));
 	leftShift lshift(.in(operand1), .shift(operand2[2:0]), .out(left_shift_result));
 	rightShift rshift(.in(operand1), .shift(operand2[2:0]), .out(right_shift_result));
 
@@ -39,29 +41,29 @@ module ALU(
 				4'b0010: begin
 					result <= sub_result;
 					flag[0] <= ~^sub_result; //Parity
-					flag[1] <= sub_carry; // Carry
+					flag[1] <= ~sub_carry; // Carry
 					flag[2] <= (sub_result == 8'b0); // Zero
 				end
 
 				//Incrementor
 				4'b0011: begin
-					result <= add_result;
-					flag[0] <= ~^add_result; //Parity
-					flag[1] <= add_carry; // Carry
-					flag[2] <= (add_result == 8'b0); // Zero
+					result <= inc_result;
+					flag[0] <= ~^inc_result; //Parity
+					flag[1] <= inc_carry; // Carry
+					flag[2] <= (inc_result == 8'b0); // Zero
 				end
 
 				//Decrementor
 				4'b0100: begin
-					result <= sub_result;
-					flag[0] <= ~^sub_result; //Parity
-					flag[1] <= sub_carry; // Carry
-					flag[2] <= (sub_result == 8'b0); // Zero
+					result <= dec_result;
+					flag[0] <= ~^dec_result; //Parity
+					flag[1] <= dec_carry; // Carry
+					flag[2] <= (dec_result == 8'b0); // Zero
 				end
 
 				//Complement
 				4'b0101: begin
-					result <= ~result;
+					result <= ~operand1;
 				end
 
 				//Left Shift
